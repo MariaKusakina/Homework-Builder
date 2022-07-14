f0fb2b465d27ef9be382d3cc1250062306e9446a
MariaKusakina <m.a.kusakina@gmail.com>
2022-07-15 00:35:17 +0300

    added new created project

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person mom = new PersonBuilder()
+                .setName("Анна")
+                .setSurname("Вольф")
+                .setAge(31)
+                .setAddress("Сидней")
+                .build();
+        Person son = mom.newChildBuilder()
+                .setName("Антошка")
+                .build();
+        System.out.println("У " + mom + " есть сын, " + son);
+
+        try {
+            new PersonBuilder().build();
+        } catch (IllegalStateException e) {
+            System.out.println("Недостаточно данных");
+        }
+
+        try {
+            new PersonBuilder().setAge(-100).build();
+        } catch (IllegalArgumentException e) {
+            System.out.println("Отрицательное число");
+        }
+    }
+}
